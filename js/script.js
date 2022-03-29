f82076b72e790b87bed8372a5de0d2ace583e92c
LuisGC21 <luis14gc18@gmail.com>
2022-03-28 23:40:34 -0600

    Js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function suma(){
+    var valor1 = parseFloat(document.getElementById('numero1').value);
+    var valor2 = parseFloat(document.getElementById('numero2').value);
+    var resultado = valor1 + valor2;
+    document.getElementById('reultado').value = resultado;
+}
+function resta(){
+    var valor1 = parseFloat(document.getElementById('numero1').value);
+    var valor2 = parseFloat(document.getElementById('numero2').value);
+    var resultado = valor1 - valor2;
+    document.getElementById('reultado').value = resultado;
+}
+function multiplicacion(){
+    var valor1 = parseFloat(document.getElementById('numero1').value);
+    var valor2 = parseFloat(document.getElementById('numero2').value);
+    var resultado = valor1 * valor2;
+    document.getElementById('reultado').value = resultado;
+}
+function division(){
+    var valor1 = parseFloat(document.getElementById('numero1').value);
+    var valor2 = parseFloat(document.getElementById('numero2').value);
+    var resultado = valor1 / valor2;
+    document.getElementById('reultado').value = resultado;
+}
